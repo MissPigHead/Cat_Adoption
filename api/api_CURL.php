93afaede07fb7ea2daa9cf2bf613666b2d5c14b6
Lingling <misspighead@gmail.com>
2021-02-16 15:33:19 +0800

    first commit

---
+++ api/api_CURL.php
@@ -0,0 +1,33 @@
+<?php
+// Cross-Origin Resource Sharing Header
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: text/html; charset=utf-8");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+
+$url="https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&animal_kind=%E8%B2%93";
+
+if(!empty($_GET['top'])){
+    $url=$url."&%24top=".$_GET['top'];
+}
+if(!empty($_GET['skip'])){
+    $url=$url."&%24skip=".$_GET['skip'];
+}
+if(!empty($_GET['animal_createtime'])){
+    $url=$url."&animal_createtime=".$_GET['animal_createtime'];
+}
+// $url="https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&%24top={$top}&%24skip={$skip}&animal_kind=%E8%B2%93";
+
+function load_contents($url)
+{
+    $curl=curl_init(); // 初始化
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl,CURLOPT_URL,$url);
+    $data=curl_exec($curl);
+
+    curl_close($curl);
+    return $data;
+}
+
+echo $content=load_contents($url);
+?>
---
+++ project.md
@@ -0,0 +1,7 @@
+# 領我回家 Take Me Home
+## API Source
+https://agridata.coa.gov.tw/api.aspx#operations-tag-%E5%AF%B5%E7%89%A9
+
+## 
+
+
